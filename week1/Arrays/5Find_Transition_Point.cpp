2063277d003be8974a71bd0e0e4b1bc3e28520a9
Arpit <111arpit1@gmail.com>
2021-08-07 01:08:53 +0530

    solved

---
+++ week1/Arrays/5Find_Transition_Point.cpp
@@ -0,0 +1,31 @@
+// Given a sorted array containing only 0s and 1s, find the transition point. 
+#include <bits/stdc++.h>
+using namespace std;
+
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    if(arr[0]==1)
+    for(int i = 0; i < n-1; i++) {
+        if(arr[i] == 0 && arr[i + 1] == 1)
+        return i+1;
+    }
+    return -1;
+}
