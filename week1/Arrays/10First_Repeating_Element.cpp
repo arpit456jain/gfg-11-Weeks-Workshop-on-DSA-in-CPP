6a47df0f5c579b3113c115d756f2b8460708eb97
Arpit <111arpit1@gmail.com>
2021-08-07 17:18:26 +0530

    solved

--- week1/Arrays/10First_Repeating_Element.cpp
+++ week1/Arrays/10First_Repeating_Element.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 class Solution{
     public:
-    int naiveApproch(int arr[],int n)
+    int optimizeApproch(int arr[],int n)
     {
         // T.C : O(N) but giving TLE
         // logic is i will take a another array and will store position of 1st occurence at that number
-        vector<int> arr2(100000000,0);
+        vector<int> arr2(1000000,0);
         int min=-1;
         for(int i = 0; i < n; i++)
         {
@@ -29,14 +29,26 @@ class Solution{
         } 
         return min;
     }
+    int naiveApproch(int arr[],int n)
+    {
+        // T.C : O(n*n)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i] == arr[j])
+                return i+1;
+            }
+        }
+    }
     //Function to return the position of the first repeating element.
     int firstRepeated(int arr[], int n) {
         //code here
-        int res =  naiveApproch(arr,n);
+        // int res =  naiveApproch(arr,n);
+        int res = optimizeApproch(arr,n);
         return res;
     }
 };
-
 // { Driver Code Starts.
 int main(){
 
