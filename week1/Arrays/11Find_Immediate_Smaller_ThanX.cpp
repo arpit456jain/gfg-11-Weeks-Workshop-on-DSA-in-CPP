69753466c8bc4b1b82a74b3c74aa8d16aeb56245
Arpit <111arpit1@gmail.com>
2021-08-08 22:54:30 +0530

    solved

---
+++ week1/Arrays/11Find_Immediate_Smaller_ThanX.cpp
@@ -0,0 +1,69 @@
+// Given an array arr[] of size N containing positive integers and an integer X, find the element in the array which is smaller than X and closest to it.
+
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // User function to complete
+    // arr[]: input array
+    // n: size of array
+    // k: find element smaller and closest to it. 
+    int immediateSmaller(int arr[], int n, int x)
+    {
+        // your code here
+        // logic is i will store 1st element in min variable then i will check every element if its smaller than x then i will check it with min 
+
+        int min = -1;
+        for (int i = 0; i < n;i++)
+        {
+            if(arr[i] < x and arr[i]>min)
+            min = arr[i];
+        }
+        
+        return min;
+    }   
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    
+            int n;
+        	cin >> n;
+        	
+        	int arr[n];
+        	
+        	for(int i = 0;i<n;i++){
+        	    cin >> arr[i];
+        	}
+        	
+        	int x;
+        	cin >> x;
+        	Solution ob;
+        	cout << ob.immediateSmaller(arr, n, x) << endl;
+	}
+	
+	
+	return 0;
+}
+  // } Driver Code Ends
+
+  /*Testcases
+  5
+4 67 13 12 15
+16
+*/
