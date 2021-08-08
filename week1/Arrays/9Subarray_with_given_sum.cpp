195dccaecca594ea0378a21557c36ae8fd74a4dd
Arpit <111arpit1@gmail.com>
2021-08-08 11:40:48 +0530

    solved

--- week1/Arrays/9Subarray_with_given_sum.cpp
+++ week1/Arrays/9Subarray_with_given_sum.cpp
@@ -11,33 +11,36 @@ class Solution
     vector<int> subarraySum(int arr[], int n, int s)
     {
         // Your code here
+        // T.C : O(n)
         // logis is i will take a window from 0 and will add elements to it until sum>=s if its graeter than s then i will remov efirst element
         vector<int>res;
         int sumOfwindow = 0;
         
         int j=0;
         int i=0;
-        while(j<n and i<=j)
+        while(j<=n)
         {
             if(sumOfwindow==s)
                 break;
-            if(sumOfwindow>s)
+            if(sumOfwindow<s)
             {
+                sumOfwindow += arr[j];
+                j++;
+                continue;
+            }
+            else{
                 sumOfwindow = sumOfwindow - arr[i];
                 i++;
                 continue;
             }
-            else{
-            sumOfwindow += arr[j];
-            j++;
-            }
             
         }
+        
         cout<<"i is"<<i<<" and j is"<<j<<"and sum is"<<s<<"and sumOfwindow is"<<sumOfwindow<<endl;
         if(sumOfwindow==s)
         {
             res.push_back(i+1);
-            res.push_back(j);
+            res.push_back(j-1);
         }
         else{
             res.push_back(-1);
