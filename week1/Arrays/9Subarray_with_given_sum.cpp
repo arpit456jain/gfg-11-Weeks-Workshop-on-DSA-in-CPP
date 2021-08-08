85376156de88be9f5640bf0907f05c03223c3be3
Arpit <111arpit1@gmail.com>
2021-08-08 10:22:41 +0530

    partial solved

--- week1/Arrays/9Subarray_with_given_sum.cpp
+++ week1/Arrays/9Subarray_with_given_sum.cpp
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+// Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S
 #include <bits/stdc++.h>
 using namespace std;
 
