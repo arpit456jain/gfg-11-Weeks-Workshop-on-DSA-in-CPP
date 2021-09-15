1fcf52da03b06fc0f4b5c59fd5e4e9b848bcd454
Arpit <111arpit1@gmail.com>
2021-09-15 21:28:41 +0530

    solved

---
+++ week1/Arrays/Merge_2_Sorted_Array_Using_Extra_Space.cpp
@@ -0,0 +1,95 @@
+/*
+Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+ 
+
+Example 1:
+
+Input: 
+n = 4, arr1[] = [1 3 5 7] 
+m = 5, arr2[] = [0 2 6 8 9]
+Output: 
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9]
+Explanation:
+After merging the two 
+non-decreasing arrays, we get, 
+0 1 2 3 5 6 7 8 9.
+*/
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+            // logic is to traverse both arrays and pic the minimium one and put it in 3rd array
+            int i=0;
+            int j=0;
+            vector<int> arr3;
+            while(i<n || j<m)
+            {
+                if (arr1[i]<arr2[j])
+                {
+                arr3.push_back(arr1[i]);
+                i++;
+                }
+
+                if (arr2[j]<arr1[i])
+                {
+                arr3.push_back(arr2[j]);
+                j++;
+                }
+            }
+            for(int i=0; i<n; i++)
+            {
+                arr1[i] = arr3[i];
+            }
+            for(int i=0; i<m; i++)
+            {
+                arr2[i] = arr3[i+m-1];
+            }
+            
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
