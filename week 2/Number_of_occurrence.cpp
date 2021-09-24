91e867ab409e2590d67c6238ab01ed113633b782
Arpit <111arpit1@gmail.com>
2021-09-24 22:08:30 +0530

    solved

--- week 2/Number_of_occurrence.cpp
+++ week 2/Number_of_occurrence.cpp
@@ -35,18 +35,59 @@ public:
         }
         return c;
     }
+    int firstOccurence(int arr[], int low,int high, int x,int n)
+    {
+        if(high>=low)
+        {
+            int mid = (low + high)/2;
+            cout<<mid<<endl;
+            if((mid == 0 || arr[mid]>arr[mid-1]) && arr[mid] == x)
+            return mid;
+            
+            else if(arr[mid]>=x)
+            return firstOccurence(arr,low,mid-1,x,n);
+
+            else
+            return firstOccurence(arr,mid+1,high,x,n);
+            
+        }
+          return -1;
+    }
+    int lastOccurence(int arr[], int low,int high, int x,int n)
+    {
+        if(high>=low)
+        {
+            int mid = (low + high)/2;
+            if((mid == n-1 || arr[mid]<arr[mid+1]) && arr[mid] == x)
+            return mid;
+
+            else if(arr[mid]>x)
+            return lastOccurence(arr,low,mid-1,x,n);
+
+            else
+            return lastOccurence(arr,mid+1,high,x,n);
+        }
+          return -1;
+    }
 	int optimizeApproch(int arr[], int n, int x) {
         // first sort the array 
         sort(arr,arr+n); //T.C : O(log(n))
         // now the array is 1 1 2 2 2 2 3 
         // we just want first and last occurence of x which is 2 in this case which also required O(log(n))
 
-        
+        // first occurence
+        int first = firstOccurence(arr,0,n-1,x,n);
+        int last = lastOccurence(arr,0,n-1,x,n);
+        cout<<"firstOccurence"<<first<<" lastOccure"<<last<<"\n";
+        int no_of_occurrences = last - first;
+        if (first == -1)
+        return 0;
+        return no_of_occurrences + 1;
     }
 	int count(int arr[], int n, int x) {
 	    // code here
         int ans ;
-        ans = naiveApproch(arr,n,x);
+        // ans = naiveApproch(arr,n,x);
         ans = optimizeApproch(arr,n,x);
         return ans;
 	}
